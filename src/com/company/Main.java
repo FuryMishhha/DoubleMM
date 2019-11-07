665a406c1d59e6076e11a2a2a101c9ea4f6f65df
mikele <mni007@yandex.ru>
2019-11-07 17:37:08 +0300

    my first code in this project

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int []a=new int[10];
+	for(int i=0;i<10;i++){
+	    a[i]=i*(i+1);
+	    System.out.print(a[i]+ " ");
+    }
+
+    }
+}
